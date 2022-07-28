3cadfa3c788a2083a994dd1eb7a998a354c3ffe7
marinpollard <marinpollards@gmail.com>
2022-07-28 14:53:45 +0300

    second commit 3 page links

--- js/header_click.js
+++
@@ -1,6 +0,0 @@
-// const green = document.querySelector('.navigation__link') 
-
-// green.addEventListener('click', () => {
-//     console.log(this);
-//     this.style.color = '$color-green'
-// });
